689f81c8f0ea23126d5d00574676eb796f1ecc53
dorthu <dorthu2@gmail.com>
2019-12-24 10:30:11 -0500

    + Ambience and Click Detection
    Merry Christmas Eve!  I stole a few minutes to myself this morning to
    keep this project moving while the holiday takes over, and I feel pretty
    good about it - first off, Josh's awesome ambient music is now playing
    by default, and secondly clicking only interacts with the clickable
    objects if the click occurs within its collision box.  Those two things
    together are a big improvement.  I think up next is getting the camera
    correct/to stop panning at the edges of the room...then maybe making
    room transitions work.

--- src/clickable.gd
+++ src/clickable.gd
@@ -5,9 +5,11 @@ extends Node2D
 # var b = "text"
 class_name Clickable
 
+# These can be set in the editor
 export var dialog_text: String = ""
 
-func _input(event):
+
+func _on_Area2D_input_event(viewport, event, shape_idx):
 	if event is InputEventMouseButton and event.pressed:
 		get_tree().set_input_as_handled()
 		get_tree().get_root().get_children()[0].show_dialog(dialog_text)
