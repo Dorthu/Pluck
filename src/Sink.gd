0c48149f43243f4c1fab27795c551aa60c451183
Will Smith <wsmith@linode.com>
2020-01-05 15:17:50 -0500

    + The Longest Night
    Last night we got to the airport to catch our flight home at 5:30 to
    find it delayed from 7:40 to 10:05.  While waiting, it was further
    delayed, _twice_, before finally being cancelled at 11.  The next flight
    we could book even close to home was on Monday, so I had to take another
    day off, Pat's missing another day of school, and we've got another
    flight to get through.  Also, I cannot recommend flying with Frontier -
    I get that things happen and flights get cancelled, but the way they
    handled it was terrible; they didn't even announce the delays at the
    terminal, and the cancellation came up on my phone long before they said
    anything.  Horrible.
    
    But it does give us more time to finish this project up!  Or, at least,
    "finish" it up.  Sara has the last two rooms sketched out, and I got the
    backpack quest fully implemented.  It even took a customer `Clickable`
    class for the `Sink`, which exchanges an item for another item.  I might
    make that more generic, if I decide I need it elsewhere.
    
    At this point, I need an opening dialog to run, the last graphic for the
    kitchen imported, to setup the initial game state, and to figure out how
    to export/host the final product.
    
    So, we've got a day.  I think we can do it.

---
+++ src/Sink.gd
@@ -0,0 +1,21 @@
+extends Clickable
+
+export var cup_of_water_texture: Texture
+export(Array, String) var fill_cup_text
+var fill_cup_text_pool: Clickable.DialogTextPool
+
+func _ready():
+	fill_cup_text_pool = Clickable.DialogTextPool.new(fill_cup_text)
+
+func show_dialog(event):
+	var controller = get_tree().get_root().get_children()[0]
+	
+	if controller.active_item != null and controller.active_item.id == 'cup':
+		# fill the cup with water
+		get_tree().set_input_as_handled()
+		controller.inventory.remove_item(controller.active_item)
+		controller.collect_item('water_cup', cup_of_water_texture)
+		controller.show_dialog(fill_cup_text_pool)
+		return
+	
+	.show_dialog(event)
