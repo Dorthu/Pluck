e390a0417e9fc85ae994bc204823eec2cccaf07d
dorthu <dorthu2@gmail.com>
2020-01-25 00:00:24 -0500

    + Bee Physics
    Tonight, in our third Game Jam Friday (Friday Night Jam?  Whatever it
    was called) Sara finished the graphics for the kitchen!  Which is really
    exicting, and I'll publish the updated game tomorrow!
    
    But that's not all - I spent like four hours working on bee physics.
    I've clearly been away from the mathy side of game development for a
    while, and after a few rough attempts at getting a bee to fly around
    correclty, I figured out an approach that should seem pretty natural:
    The bee has a point he wants to go, and an angle he's currently
    travelling in.  Each step, he takes figures out the angle he wants to be
    travelling, then turns as far as he can in that direction.  His velocity
    increases if he's not turning much, but decreases the harder the turn
    was.  The point he wants to reach is fixed, but he'll avoid your mouse
    if possible unless you're holding the item he wants, in which case he'll
    travel toward your mouse instead.
    
    That sounds pretty reasonable, but I think I'm doing something wrong
    geting the angle between two points, as it isn't working like I expect.
    
    I also got an issue opened on github earlier this week asking for a
    license to be added to the game's repo, which is wild; I didn't realize
    anyone would care to look at the source.  So that's coming soon too I
    guess.
    
    It's now after midnight, my energy drink (bang! if you were wodnering)
    is wearing off, and I'm turning in.  Update should be published
    tomorrow.

--- src/bumble.gd
+++ src/bumble.gd
@@ -8,14 +8,20 @@ class_name Bumble
 var controller # The MasterController 
 
 const MOVE_SPEED = 500
+const MIN_SPEED := 20.0
+const MAX_SPEED := 500.0
 var room_center: Vector2
 var velocity := Vector2(0, -1)
+
+var angle := 1.57
+var cvelocity := 1.0
+const MAX_TURN := 3.0
 
 func _ready():
 	controller = get_tree().get_root().get_children()[0]
 	room_center = Vector2(get_parent().room_width/2, 300)
 
-func _process(delta):
+func old_process(delta):
 	var mouse_pos = get_global_mouse_position()
 	# if I'm on the screen, what am I doing?
 	if controller.active_item != null and controller.active_item.id == 'liquor':
@@ -31,3 +37,64 @@ func _process(delta):
 			velocity = (Vector2(500, 300) - self.position).normalized()
 	
 	self.position += velocity*MOVE_SPEED*delta
+
+func _process(delta):
+	# find where we want to go
+	var goal := Vector2(500, 300) # arbitrary point to buzz around
+	var mouse_pos = get_global_mouse_position()
+	var avoid_mouse = true
+	
+	# maybe we want to go to the mouse instead of there
+	if controller.active_item != null and controller.active_item.id == 'liquor':
+		avoid_mouse = false
+		goal = mouse_pos
+	
+	# TODO: I _think_ this is where the issue is
+	# right now we're getting stuck looping around random points
+	# and I think it's because this angle isn't correct
+	var angle_to_goal := (goal - self.position).normalized().angle()
+	
+	# oh no wait we want to avoid the mouse
+	if avoid_mouse:
+		var distance_to_mouse = self.position.distance_to(mouse_pos)
+		
+		# it's too close - run (fly?) away!
+		if distance_to_mouse < 200:
+			# flying away is deciding we want to fly _exactly away_
+			angle_to_goal = deg2rad(180) + (mouse_pos - self.position).normalized().angle()
+	
+	print(rad2deg(angle_to_goal))
+	
+	# this is how far we can turn this step (include delta for smoothness)
+	var max_turn_step = MAX_TURN * delta
+	
+	# so this is how hard I want to turn
+	var desired_turn = abs(angle_to_goal - angle)
+	
+	# now break it down into how hard I can actually turn and in what direction
+	var turn_dir = -1 if (angle_to_goal - angle) < 0 else 1
+	if desired_turn > max_turn_step:
+		desired_turn = max_turn_step
+	
+	# turn!
+	angle += turn_dir * desired_turn
+	
+	# keep our angle clamped to within 0-360
+	if rad2deg(angle) < 0:
+		angle = deg2rad(rad2deg(angle) + 360)
+	elif rad2deg(angle) > 360:
+		angle = deg2rad(rad2deg(angle) - 360)
+	
+	# slow down when turning hard, speed up when flying straight
+	if desired_turn > max_turn_step * 1/4:
+		# slow down
+		cvelocity -= desired_turn * 10 * delta
+		cvelocity = max(MIN_SPEED, cvelocity)
+	else:
+		# speed up
+		cvelocity += cvelocity * delta
+		cvelocity = min(MAX_SPEED, cvelocity)
+	
+	# actually move in the direction we're going
+	var move_dir = Vector2(cos(angle), sin(angle)).normalized()
+	self.position += move_dir * cvelocity * delta
